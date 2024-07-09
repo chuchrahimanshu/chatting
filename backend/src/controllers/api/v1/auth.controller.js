ffa80d80a35139d7491467a45d3cec17d1842f3a
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-09 17:54:49 +0530

    Write Sign Up Controller Action in Auth Module

--- backend/src/controllers/api/v1/auth.controller.js
+++ backend/src/controllers/api/v1/auth.controller.js
@@ -1,5 +1,36 @@
+import { User } from "../../../models/user.model.js";
+
 export const signUp = async (req, res) => {
   try {
+    const { firstName, lastName, email, password } = req.body;
+    if (
+      !firstName?.trim() ||
+      !lastName?.trim() ||
+      !email?.trim() ||
+      !password?.trim()
+    ) {
+      return res.status(400).json({
+        message: "Please provide all required details",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        message: "User already registered, Please Sign In",
+      });
+    }
+
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Error - Auth Module - Sign Up Controller",
