ed5a3b8c12e2132cda5c47eb4f609cbe3f914e8a
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-09 18:03:36 +0530

    Implement Validations to all tokens in Token Schema

--- backend/src/models/token.model.js
+++ backend/src/models/token.model.js
@@ -1,5 +1,6 @@
 // Import Section
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 // Schema Section
 const tokenSchema = new mongoose.Schema({
@@ -33,6 +34,40 @@ const tokenSchema = new mongoose.Schema({
     },
   },
 });
+
+// Pre Save Section
+tokenSchema.pre("save", async function (next) {
+  if (this.isModified("forgetPassword")) {
+    const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS?.toString();
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    this.forgetPassword = await bcrypt.hash(this.forgetPassword, salt);
+  }
+
+  if (this.isModified("tfa")) {
+    const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS?.toString();
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    this.tfa = await bcrypt.hash(this.tfa, salt);
+  }
+
+  if (this.isModified("emailVerification")) {
+    const SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS?.toString();
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    this.emailVerification = await bcrypt.hash(this.emailVerification, salt);
+  }
+
+  next();
+});
+
+// Schema Methods Section
+tokenSchema.methods.validateForgetPasswordToken = async function (token) {
+  return await bcrypt.compare(token, this.forgetPassword);
+};
+tokenSchema.methods.validateTFAToken = async function (token) {
+  return await bcrypt.compare(token, this.tfa);
+};
+tokenSchema.methods.validateEmailVerificationToken = async function (token) {
+  return await bcrypt.compare(token, this.emailVerification);
+};
 
 // Exporting and Creating Model Section
 const Token = mongoose.model("Token", tokenSchema);
