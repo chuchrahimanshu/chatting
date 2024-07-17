849f7e276ed0359eabbc72d8431871eedb56b290
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-17 17:28:07 +0530

    Create Chat Schema Model

---
+++ backend/src/models/chat.model.js
@@ -0,0 +1,29 @@
+// Import Section
+import mongoose from "mongoose";
+
+// Schema Section
+const chatSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Exporting and Creating Model Section
+const Chat = mongoose.model("Chat", chatSchema);
+export { Chat };
