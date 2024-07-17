7fbb181c2b84c742cd38ab225849ebdd904e0f6b
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-17 17:22:15 +0530

    Setup Web Sockets using Socket.io and HTTP

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 // Import Section
 import express from "express";
+import { createServer } from "node:http";
 import "dotenv/config";
 import "./config/database.config.js";
 import "./config/nodemailer.config.js";
 import router from "./routes/index.routes.js";
 import morgan from "morgan";
 import cors from "cors";
+import { Server } from "socket.io";
 
 // Configuration Section
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT;
 
 // Middleware Section
@@ -24,6 +28,20 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routing and Port Listening Section
 app.use("/", router);
+
+io.on("connection", (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+
+  socket.on("message", (msg) => {
+    console.log(`Message from ${socket.id}: ${msg}`);
+    io.emit("message", { id: socket.id, text: msg });
+  });
+});
+
 app.listen(PORT, (error) => {
   if (error) {
     console.log(`Error - Connecting to Server at http://localhost:${PORT}`);
