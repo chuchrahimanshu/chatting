3fb37f70972592d3ebc8ce5b8fdd4ed3efb0903e
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-07-17 17:35:01 +0530

    Create Group Chat Schema Model

---
+++ backend/src/models/group.chat.model.js
@@ -0,0 +1,24 @@
+// Import Section
+import mongoose from "mongoose";
+
+// Schema Section
+const groupChatSchema = new mongoose.Schema(
+  {
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Exporting and Creating Model Section
+const GroupChat = mongoose.model("GroupChat", groupChatSchema);
+export { GroupChat };
